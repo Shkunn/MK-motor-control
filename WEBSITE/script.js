017515a1761527aa7f720c41f37bf6014916d24c
Teddy <leclercqted@gmail.com>
2021-02-03 00:28:41 +0100

    CODE
    [UDPATE]

--- WEBSITE/script.js
+++ WEBSITE/script.js
@@ -1,7 +1,31 @@
-function fonction1() {
-    alert('Bravo ');
+const udp = requirejs('dgram')
+const PORT = 9000
+const HOST = "192.168.220.81"
+
+// creating a client socket
+const client = udp.createSocket('udp4')
+
+const data_UP = Buffer.from('MSG from UDP client')
+const data_DOWN = Buffer.from('Polpi')
+
+function fonction1(PORT, HOST, client, data_UP) {
+    client.send(data_UP, PORT, HOST, error => {
+        if (error) {
+            console.log(error)
+            client.close()
+        } else {
+            console.log('Data sent !!!')
+        }
+    })   
 }
 
-function fonction2() {
-    alert('heloloolo');
+function fonction2(PORT, HOST, client, data_DOWN) {
+    client.send(data_UP, PORT, HOST, error => {
+        if (error) {
+            console.log(error)
+            client.close()
+        } else {
+            console.log('Data sent !!!')
+        }
+    })   
 }
