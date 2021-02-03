300e603abd02bdf59b33aa327c14e1a48d6b716d
Teddy <leclercqted@gmail.com>
2021-02-03 09:02:11 +0100

    [UDPATE]
    all scripts

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-var udp = requirejs('dgram')
+var dgram = require('dgram');
 
 const PORT = 9000
 const HOST = "192.168.220.81"
 
 // creating a client socket
-const client = udp.createSocket('udp4')
+const client = dgram.createSocket('udp4')
 
 const data_UP = Buffer.from('MSG from UDP client')
 const data_DOWN = Buffer.from('Polpi')
@@ -21,7 +21,7 @@ function fonction1(PORT, HOST, client, data_UP) {
 }
 
 function fonction2(PORT, HOST, client, data_DOWN) {
-    client.send(data_UP, PORT, HOST, error => {
+    client.send(data_DOWN, PORT, HOST, error => {
         if (error) {
             console.log(error)
             client.close()
