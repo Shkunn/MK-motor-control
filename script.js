c4a313aa8068c5ab52d7ab780bb7230e3542d388
Shkunn <36813699+Shkunn@users.noreply.github.com>
2021-02-03 07:04:22 +0100

    files to root
    [MOOVING] files to root to create a github page

---
+++ script.js
@@ -0,0 +1,36 @@
+var udp = requirejs('dgram')
+
+const PORT = 9000
+const HOST = "192.168.220.81"
+
+// creating a client socket
+const client = udp.createSocket('udp4')
+
+const data_UP = Buffer.from('MSG from UDP client')
+const data_DOWN = Buffer.from('Polpi')
+
+function fonction1(PORT, HOST, client, data_UP) {
+    client.send(data_UP, PORT, HOST, error => {
+        if (error) {
+            console.log(error)
+            client.close()
+        } else {
+            console.log('Data sent !!!')
+        }
+    })   
+}
+
+function fonction2(PORT, HOST, client, data_DOWN) {
+    client.send(data_UP, PORT, HOST, error => {
+        if (error) {
+            console.log(error)
+            client.close()
+        } else {
+            console.log('Data sent !!!')
+        }
+    })   
+}
+
+function fonction3() {
+    alert('HEHO CLICK')
+}
